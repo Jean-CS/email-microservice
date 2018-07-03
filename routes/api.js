24e1c58a547173e2b82c2bf541e832e9d7b04e35
Jean Santos <jeansantosc@gmail.com>
2018-07-02 22:35:45 -0300

    add: send email through post request

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,49 @@ var router = express.Router();
 
 var helper = require('sendgrid').mail;
 
+router.post(":action", function (req, res, next) {
+
+    var action = req.params.action;
+
+    if (action == 'send') {
+        var from_email = new helper.Email(process.env.FROM_EMAIL);
+        var to_email = new helper.Email(req.body.recipient);
+        var subject = req.body.subject;
+        var content = new helper.Content('text/html', req.body.confirmation);
+        var mail = new helper.Mail(from_email, subject, to_email, content);
+
+        var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+        var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON(),
+        });
+
+        sg.API(request, function (error, response) {
+            if (error) {
+                res.json({
+                    confirmation: 'Fail',
+                    message: error
+                });
+
+                return;
+            }
+
+            res.json({
+                confirmation: 'Success',
+                response: response
+            });
+        });
+
+        return;
+    }
+
+    res.json({
+        confirmation: 'Fail',
+        message: 'Invalid action'
+    });
+
+});
 
 // send email
 router.get('/:action', function (req, res, next) {
